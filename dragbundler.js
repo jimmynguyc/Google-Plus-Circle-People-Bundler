52be801e8c52a964d846e595c1bf4c1da13c867e
Jimmy Ngu <jimmynguyc@gmail.com>
2011-07-01 09:32:32 +0800

    smoother initial animation

--- dragbundler.js
+++ dragbundler.js
@@ -1,4 +1,6 @@
 $(function(){
+	var currentDragPosition;
+	
 	$('div.people').each(function(){
 		$(this).prop('email',$(this).children('div.email').html());
 	}).mousedown(function(){
@@ -10,13 +12,13 @@ $(function(){
 		stop: returnToPosition
 	});
 	
-	function startDrag(){
+	function startDrag(event,ui){
 		$(this).addClass('selected');
-	
+		currentDragPosition = ui.position;
 	}
 	
 	function cloneHelper(){
-		var helper = $(this).clone().addClass('clone-master').addClass('clone').css('z-index',999);
+		var helper = $(this).clone().addClass('clone-master clone').css('z-index',999);
 		var total = $('.people.selected:not(.clone)').length;
 		if(total > 1){
 			$(helper).append($('<div class="nametag">' + total + ' people</div><div class="paperclip"></div>'));
@@ -26,11 +28,14 @@ $(function(){
 	
 	function bundleTogether(event,ui){
 		var element = this;
+		currentDragPosition = ui.position;
 		$('div.people.selected:not(.clone)').each(function(i,el){
 			var found = false;
 			$('.clone').each(function(j,cl){
 				if($(cl).prop('email') == $(el).prop('email')){
-					$(cl).css('z-index',998-i).stop(false,true).animate(ui.position, 300);
+					if(!$(cl).hasClass('firstmove')){
+						$(cl).stop(true,true).css('z-index',998-i).css(ui.position);
+					}
 					found = true;
 				}
 			});
@@ -38,11 +43,24 @@ $(function(){
 				var rot = (Math.random() - 0.5) * 15;
 				$(el).clone()
 						.css($(el).offset())
-						.animate(ui.position, 300)
-						.addClass('clone')
+						.addClass('clone firstmove')
+						.animate(ui.position, 
+									{ 	duration:200, 
+										step: function(now, fx){
+											if(fx.prop == 'top'){
+												fx.end = currentDragPosition.top;
+											}
+											if(fx.prop == 'left'){
+												fx.end = currentDragPosition.left;
+											}
+										},
+										complete:function(){ 
+											$(this).removeClass('firstmove');
+										}
+									})
 						.prop('email',$(el).prop('email'))
 						.appendTo(el.parentNode)
-						.css({ 	'z-index': 999-i,
+						.css({ 	'z-index': 998-i,
 								'position':'absolute',
 								'-webkit-transform': 'rotate('+rot+'deg)',
 								'-moz-transform': 'rotate('+rot+'deg)'});
